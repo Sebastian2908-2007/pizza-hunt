72dd5871ca1e900b151cc49e7b8f69d27c3e2212
sebastian <sebastian bowen>
2021-05-22 21:52:45 -0600

    can now reply to comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,33 @@
-const {Schema, model} = require('mongoose');
+const {Schema, model, Types} = require('mongoose');
+const dateFormat= require('../utils/dateFormat');
+
+const ReplySchema =new Schema(
+    {
+        // set custom id to avoid confusion with parent comment
+        replyId: {
+         type: Schema.Types.ObjectId,
+         default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String 
+        },
+        writtenBy: {
+            type: String 
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+      }
+);
 
 
 const CommentSchema = new Schema({
@@ -10,9 +39,24 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
-});
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  })
 
 const Comment = model('Comment', CommentSchema);
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {addComment, removeComment} = require('../../controllers/comment-controller');
+const {addComment, removeComment, addReply, removeReply} = require('../../controllers/comment-controller');
 
 // /api/comments/:pizzaId
 router
@@ -8,6 +8,11 @@ router
 // /api/comments/pizzaId/commentsId
 router
 .route('/:pizzaId/:commentId')
+.put(addReply)
 .delete(removeComment);
+
+router
+.route('/:pizzaId/:commentId/replyId')
+.delete(removeReply);
 
 module.exports = router;
